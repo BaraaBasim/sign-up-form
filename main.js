11c9db6ede8f0728b7bade5dd117bc435807a557
Baraa Basim <barabasim77@gmail.com>
2023-09-09 14:19:49 +0800

    push error

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   let password = document.querySelector("#password").value;
-  let confPassword = document.querySelector("confPassword").value;
+  let confPassword = document.querySelector("#confPassword").value;
   let error = document.querySelector("#error");
 
   if (password === "" && confPassword === "") {
