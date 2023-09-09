07d928c1313f487cca66483356e531f6d93f19df
Baraa Basim <barabasim77@gmail.com>
2023-09-09 11:41:04 +0800

    submit btn

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   let confPassword = document.querySelector("confPassword").value;
   let error = document.querySelector("#error");
 
-  if (password != confPassword) {
+  if (password === "" && confPassword === "") {
     error.innerText = "*passwords do not match";
+  } else if (password === confPassword) {
+    error.innerText = "";
   }
 });
